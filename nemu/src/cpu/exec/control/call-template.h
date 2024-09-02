8fb6b6baee48800cf402484efb75ebfcafa944d2
ulmfle <vnlsrt@gmail.com>
2024-09-02 23:15:29 +0800

    before starting pa2

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,10 +2,13 @@
 
 #define instr call
 
-// static void do_execute() {
-//     cpu.eip += op_src->val;
-//     print_asm_template2();
-// }
+static void do_execute() {
+    cpu.eip += op_src->val;
+    print_asm_template2();
+}
 
+make_helper(concat(call_rel_,SUFFIX)) {
+    
+}
 
 #include "cpu/exec/template-end.h"
