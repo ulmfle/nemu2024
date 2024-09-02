0f0ad7e207b717367c947474b427be471323076a
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-02 22:52:22 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:52:22 up 1 day,  9:36,  2 users,  load average: 1.49, 0.98, 0.81 7b8487376603ba3865f4726b6544de60d37f462f

---
+++ nemu/src/cpu/exec/control/call-template.h
@@ -0,0 +1,11 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+// static void do_execute() {
+//     cpu.eip += op_src->val;
+//     print_asm_template2();
+// }
+
+
+#include "cpu/exec/template-end.h"
