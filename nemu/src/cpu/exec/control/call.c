7b96a4297ad19170743ce16f1a59be968316a7e2
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 11:57:36 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:57:36 up  2:47,  1 user,  load average: 0.48, 0.31, 0.26 d6c5e14b5f9c6d43a0de6cce97aa0f320f453e6

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -1,11 +1,13 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 2
-#include "call-template.h"
-#undef DATA_BYTE
+// #define DATA_BYTE 2
+// #include "call-template.h"
+// #undef DATA_BYTE
 
 #define DATA_BYTE 4
 #include "call-template.h"
 #undef DATA_BYTE
 
-make_helper_v(call_rel);
+make_helper(call_rel_v) {
+    return call_rel_l(eip);
+}
