9d75316ecc27909d4577afb0d7403bee2747862b
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 23:44:56 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:44:56 up  1:05,  1 user,  load average: 0.34, 0.28, 0.27 c4d0e98784d5dec96f291ef0ef34ca55569d357d

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -3,8 +3,8 @@
 #define instr ret
 
 make_helper(ret) {
-    cpu.eip = swaddr_read(cpu.esp, 4);
+    uint32_t addr = swaddr_read(cpu.esp, 4);
     cpu.esp += 4;
     print_asm("ret");
-    return 0;
+    return addr - cpu.eip;
 }
