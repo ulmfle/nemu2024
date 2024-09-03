9f802d5182b498fe3a854f311f358e67fad19796
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 11:42:27 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:42:27 up  2:31,  1 user,  load average: 0.47, 0.31, 0.22 e998fb06df52cf3bf563649f6fbf43a843d354cc

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -4,7 +4,12 @@
 
 make_helper(concat(call_rel_,SUFFIX)) {
     int len = decode_si_l(eip + 1);
+
+    REG(R_ESP) -= 4;
+    MEM_W(REG(R_ESP), eip);
+
     cpu.eip += op_src->simm;
+
     print_asm_template1();
     return len + 1;
 }
