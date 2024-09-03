4f89740f7d1e073a71401819a65e4be447dc8ade
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 10:06:32 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:06:32 up 55 min,  1 user,  load average: 0.54, 0.43, 0.34 9a9496f7371e82d2240588d0497f04fc712a7a90

---
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr push
+
+static void do_execute() {
+    REG(R_ESP) -= DATA_BYTE;
+    REG(R_ESP) = op_src->val;
+    print_asm_template1();
+}
+
+make_instr_helper(r);
+make_instr_helper(i);
+
+#include "cpu/exec/template-end.h"
