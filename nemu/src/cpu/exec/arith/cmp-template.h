9dab8176fc306426bb2d9c8ea8fa3410f6aaeb74
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-06 15:55:29 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:55:29 up  2:46,  1 user,  load average: 0.60, 0.60, 0.43 52ef98ca892b8eee4e43c97873907c44996ddd70

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -8,7 +8,7 @@ static void do_execute() {
     update_eflags_pf_zf_sf((DATA_TYPE_S)result);
 	cpu.eflags.CF = ((op_dest->val >> (8*DATA_BYTE - 2)) & 1) != ((result >> (8*DATA_BYTE - 2)) & 1);
 	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
-    printf("result : %u, src = %u; reg = %d, dest = %u\n", result, op_dest->val, op_dest->reg, op_src->val);
+    //printf("result : %u, src = %u; reg = %d, dest = %u\n", result, op_dest->val, op_dest->reg, op_src->val);
     print_asm_template2();
 }
 
@@ -16,9 +16,31 @@ static void do_execute() {
 make_instr_helper(si2rm);
 #endif
 
-make_instr_helper(i2r);
 make_instr_helper(i2rm);
 make_instr_helper(r2rm);
 make_instr_helper(rm2r);
+
+#if DATA_BYTE == 1
+make_helper(cmp_i2al_b) {
+    DATA_TYPE result = REG(0) - op_src->val;
+
+    update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = ((op_dest->val >> (8*DATA_BYTE - 2)) & 1) != ((result >> (8*DATA_BYTE - 2)) & 1);
+	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+    print_asm_template2();
+    return 2;
+}
+#else
+make_helper(concat(cmp_i2eax_, SUFFIX)) {
+    DATA_TYPE result = REG(0) - op_src->val;
+
+    update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = ((op_dest->val >> (8*DATA_BYTE - 2)) & 1) != ((result >> (8*DATA_BYTE - 2)) & 1);
+	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+    print_asm_template2();
+    return 2;
+}
+
+#endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -12,8 +12,7 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
-
-make_helper_v(cmp_i2r);
+make_helper_v(cmp_i2eax);
 make_helper_v(cmp_i2rm);
 make_helper_v(cmp_r2rm);
 make_helper_v(cmp_rm2r);
