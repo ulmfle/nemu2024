da6380f6bd5323de086b983335e712d1db12e089
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 00:39:19 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:39:19 up  1:32,  1 user,  load average: 0.61, 0.36, 0.31 a798774b524b2adbfcf48676684c902ab1e24350

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -17,10 +17,19 @@ static void do_execute() {
 make_instr_helper(si2rm);
 #endif
 
-make_instr_helper(i2r);
 make_instr_helper(i2rm);
 make_instr_helper(r2rm);
 make_instr_helper(rm2r);
 
+make_helper(concat(add_i2eax_, SUFFIX)) {
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
+
+    DATA_TYPE result = REG(0) + op_src->val;
+    update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = ((op_dest->val >> (8*DATA_BYTE - 2)) & 1) != ((result >> (8*DATA_BYTE - 2)) & 1);
+	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+    print_asm(str(instr) str(SUFFIX) " %s,%%%s", op_src->str, REG_NAME(0));
+    return len + 1;
+}
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/add.c
+++ nemu/src/cpu/exec/arith/add.c
@@ -12,7 +12,7 @@
 #include "add-template.h"
 #undef DATA_BYTE
 
-make_helper_v(add_i2r);
+make_helper_v(add_i2eax);
 make_helper_v(add_i2rm);
 make_helper_v(add_r2rm);
 make_helper_v(add_rm2r);
