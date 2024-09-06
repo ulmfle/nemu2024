6390edab500098d845ec8e97aead1d86da880611
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 01:51:45 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:51:45 up  2:44,  1 user,  load average: 0.67, 0.38, 0.31 ac4664e4df09ec436f99ea4892555926abf3a4b1

--- testcase/src/extra/exadd.c
+++ testcase/src/extra/exadd.c
@@ -1,17 +1,15 @@
 #include "trap.h"
 
-// int NONAME(int v) {
-//     return v;
-// }
+int NONAME(int v) {
+    return v;
+}
 
 int main() {
-    //int a = NONAME(1);
+    int a = NONAME(1);
     //int b = NONAME(2);
-    int a = 1;
-    int b = 2;
+    //int a = 1;
+    //int b = 2;
     nemu_assert(a==1);
-    nemu_assert(b==2);
-    int c = a+b;
-    nemu_assert(c == a+b);
+    //nemu_assert(b==2);
     return 0;
 }
