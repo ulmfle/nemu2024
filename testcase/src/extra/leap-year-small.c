4a516a3eb325959badd1595da12ecbf54c5e927e
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 02:41:13 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:41:13 up  3:34,  1 user,  load average: 0.77, 0.56, 0.72 24091260ba52b852edd98f05e55f1676f3d02d53

--- testcase/src/extra/leap-year-small.c
+++ testcase/src/extra/leap-year-small.c
@@ -9,12 +9,13 @@ int ans[] = {0, 0, 1};
 #define NR_DATA (sizeof(ans) / sizeof(ans[0]))
 
 int main() {
-	int i;
-	for(i = 0; i < NR_DATA; i ++) {
-		nemu_assert(is_leap_year(i + 1890) == ans[i]);
-	}
+	//for(i = 0; i < NR_DATA; i ++) {
+		nemu_assert(is_leap_year(0 + 1890) == ans[0]);
+		nemu_assert(is_leap_year(1 + 1890) == ans[1]);
+		nemu_assert(is_leap_year(2 + 1890) == ans[2]);
+	//}
 
-	nemu_assert(i == NR_DATA);
+	//nemu_assert(i == NR_DATA);
 
 	return 0;
 }
