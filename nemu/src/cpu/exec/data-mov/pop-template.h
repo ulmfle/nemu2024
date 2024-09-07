6269fdf3e375cd2439f3c881e392a8920c9ed92d
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 16:36:05 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:36:05 up  2:25,  1 user,  load average: 0.35, 0.19, 0.18 3d291b008a296b71767a262f569b1e7252ace2ec

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,11 +3,11 @@
 #define instr pop
 
 static void do_execute() {
-    POP(REG(op_src->reg));
+    if (op_src->type == OP_TYPE_REG) POP(REG(op_src->reg));
+    else POP(REG(op_src->addr));
     print_asm_template1();
 }
 
-make_instr_helper(r);
 make_instr_helper(rm);
-
+make_instr_helper(r);
 #include "cpu/exec/template-end.h"
