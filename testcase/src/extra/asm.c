ee25051f6f3f09f77bfdb8919558e21e6c021e98
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 15:52:14 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:52:14 up  1:41,  1 user,  load average: 0.14, 0.27, 0.16 d5e8b8d040a0850d203e91bd385126f1d63ae81d

--- testcase/src/extra/asm.c
+++
@@ -1,4 +0,0 @@
-int main() {
-    asm volatile ("movl 0x100628,%edx");
-    return 1;
-}
--- testcase/src/extra/strcmp-small.c
+++
@@ -1,7 +0,0 @@
-#include "trap.h"
-#include <string.h>
-
-int main() {
-	nemu_assert(strcmp("a", "a") == 1);
-	return 0;
-}
