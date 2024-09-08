eeb23232f141cce05c5200e26babb6e3310ef1ab
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-08 16:02:51 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:02:51 up 51 min,  1 user,  load average: 0.18, 0.15, 0.11 48ba25db28d7b5f978ea598895662cfa6712176e

--- testcase/src/extra/o.c
+++ testcase/src/extra/o.c
@@ -1,7 +1,7 @@
 #include "trap.h"
 
 int main() {
-    asm volatile ("mov $0x0,%eax");
+    asm volatile ("mov $0x7fffffff,%eax");
     asm volatile ("sub $0x1,%eax");
     asm volatile ("seto %cl");
     asm volatile ("setc %bl");
