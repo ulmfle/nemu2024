835c4406fefcaf0f03adbb141635c36472e0d60a
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-08 16:02:05 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:02:05 up 50 min,  1 user,  load average: 0.14, 0.14, 0.11 7386a1718f75d84e8c1fe86bb22a8c47859bfd05

--- testcase/src/extra/o.c
+++ testcase/src/extra/o.c
@@ -2,7 +2,7 @@
 
 int main() {
     asm volatile ("mov $0x0,%eax");
-    asm volatile ("add $0x1,%eax");
+    asm volatile ("sub $0x1,%eax");
     asm volatile ("seto %cl");
     asm volatile ("setc %bl");
     return 0;
