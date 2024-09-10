e1c5ba5850ef93d725c643b2223f972129a18ac0
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-10 10:44:09 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:44:09 up  2:23,  1 user,  load average: 0.43, 0.48, 0.41 8915ee8fc908ce2581e31f3a3718fb66963a1dc7

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -4,8 +4,8 @@
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
-	FLOAT a = f2F(0);
-	nemu_assert(a == 0);
+	FLOAT a = f2F(1);
+	nemu_assert(a == 1 << 16);
 	// FLOAT b = f2F(0.5);
 	// FLOAT c = f2F(-0.5);
 
