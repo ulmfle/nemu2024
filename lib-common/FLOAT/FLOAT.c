f2ad84f239cc19b3d9a338bf05bee32a85c51843
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-10 15:28:24 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:28:24 up  7:07,  1 user,  load average: 0.14, 0.09, 0.16 fd1bfa68169985119851a71df5bb4b3e73826184

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -28,7 +28,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	// unsigned long long a_un = (unsigned long long)Fabs(a) << 16;
 	unsigned a_un = Fabs(a);
 	unsigned b_un = Fabs(b);
-	unsigned a_un_l = a_un >> 17, a_un_r = a_un << 15;
+	unsigned a_un_l = a_un >> 16, a_un_r = a_un << 16;
 	FLOAT r;
 	// asm volatile ("div %2" : "=a"(r) : "a"((unsigned)(a_un)), "r"(b_un) , "d"((unsigned)(a_un >> 32)));
 	asm volatile ("div %2" : "=a"(r) : "a"(a_un_r), "r"(b_un) , "d"(a_un_l));
@@ -60,7 +60,7 @@ FLOAT f2F(float a) {
 
 	if (!M) return 0;
 
-	FLOAT R = E >= 8 ? M << (E-8) : M >> (8-E);
+	FLOAT R = E >= 7 ? M << (E-7) : M >> (7-E);
 	return (_a >> 31) ? -R : R;
 }
 
