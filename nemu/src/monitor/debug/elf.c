a68099a0ad4ff3d8e62dd1d6e3707a2badcaa040
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-10 23:03:18 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:03:18 up  5:25,  1 user,  load average: 0.57, 0.44, 0.32 8d350778e8f6f48ee1828f98da5756e9c6270b2f

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -88,8 +88,8 @@ swaddr_t get_symbol_swaddr(char *iden, uint8_t filter) {
 		bind = ELF32_ST_BIND(symtab[symtab_idx].st_info);
 		type = ELF32_ST_TYPE(symtab[symtab_idx].st_info);
 
-		Log("0x%08x, no, %s", symtab[symtab_idx].st_value, strtab + symtab[symtab_idx].st_name);
 		if (bind != ELF32_ST_BIND(filter) || type != ELF32_ST_TYPE(filter)) continue;
+		Log("0x%08x, no, %s", symtab[symtab_idx].st_value, strtab + symtab[symtab_idx].st_name);
 		if (strcmp(strtab + symtab[symtab_idx].st_name, iden) != 0) continue;
 
 		Log("0x%08x, yes", symtab[symtab_idx].st_value);
