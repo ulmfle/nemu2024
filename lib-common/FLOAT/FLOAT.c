d10e51de3c4d7140cd8d5857693305f5a6472412
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-10 12:32:57 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:32:57 up  4:12,  1 user,  load average: 0.08, 0.24, 0.25 5201cba5623eec8bc01a28174892a60d61b4e2fa

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -28,7 +28,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	FLOAT b_un = Fabs(b);
 	FLOAT r;
 
-	asm volatile ("div %2" : "=a"(r) : "a"((int)(a_un >> 32)), "r"(b_un) , "d"((int)(a_un)));
+	asm volatile ("div %2" : "=a"(r) : "a"((int)(a_un)), "r"(b_un) , "d"((int)(a_un >> 32)));
 
 	return (a >> 31) ^ (b >> 31) ? -r : r;
 }
