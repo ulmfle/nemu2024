237cdfe28489d840d89722890111d12987170725
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-10 13:45:47 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:45:47 up  5:25,  1 user,  load average: 0.51, 0.45, 0.36 f6777578a12ad1c47c62ff403b74d37fc639792a

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -13,4 +13,15 @@ make_helper(concat(call_rel_,SUFFIX)) {
     return len + 1;
 }
 
+make_helper(concat(call_rm_,SUFFIX)) {
+    int len = concat(decode_rm_, SUFFIX)(eip + 1);
+
+    PUSH(eip + len + 1);
+
+    cpu.eip += REG(op_src->reg) & ~0u >> (DATA_BYTE == 2 ? 16 : 0);
+
+    print_asm_template1();
+    return len + 1;
+}
+
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -1,13 +1,12 @@
 #include "cpu/exec/helper.h"
 
-// #define DATA_BYTE 2
-// #include "call-template.h"
-// #undef DATA_BYTE
+#define DATA_BYTE 2
+#include "call-template.h"
+#undef DATA_BYTE
 
 #define DATA_BYTE 4
 #include "call-template.h"
 #undef DATA_BYTE
 
-make_helper(call_rel_v) {
-    return call_rel_l(eip);
-}
+make_helper_v(call_rel);
+make_helper_v(call_rm);
