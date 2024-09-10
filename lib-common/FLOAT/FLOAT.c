29128dfff5087f9d93793c86fa210e7fbe8667cb
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-10 14:51:21 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:51:21 up  6:30,  1 user,  load average: 0.55, 0.29, 0.26 90ce563113033e6d92b0cfaf534d29702d099b5a

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -25,8 +25,9 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 * out another way to perform the division.
 	 */
 
-	// unsigned long long a_un = Fabs(a) << 16;
-	unsigned a_un = Fabs(a), b_un = Fabs(b);
+	unsigned long long a_un = Fabs(a) << 16;
+	//unsigned a_un = Fabs(a);
+	unsigned b_un = Fabs(b);
 	//unsigned a_un_l = a_un >> 17, a_un_r = a_un << 15;
 	FLOAT r;
 	asm volatile ("div %2" : "=a"(r) : "a"((unsigned)(a_un)), "r"(b_un) , "d"((unsigned)(a_un >> 32)));
