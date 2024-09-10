89e4ad538c2da4250ff7898f5fe0ddee132f3d97
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-11 01:31:33 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:31:33 up  7:53,  1 user,  load average: 1.24, 0.60, 0.41 d84cfe7f55d39f68b2a091ed89aee93dbbf172ff

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -221,7 +221,7 @@ static int cmd_bt(char *args) {
 		now_ebp += 4;
 		f_args[3] = swaddr_read(now_ebp, 4);
 
-		func_addr = ret_addr + (int)swaddr_read(ret_addr - 4, 4);
+		func_addr = ret_addr;// + (int)swaddr_read(ret_addr - 4, 4);
 		// printf("0x%08x | %s : ( %u , %u , %u , %u )\n", func_addr 
 		// 											  , get_symbol_name(func_addr)
 		// 											  , f_args[0]
