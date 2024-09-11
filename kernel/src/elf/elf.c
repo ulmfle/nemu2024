2b8335cb9462479634c67814b0dc1460c43ab29d
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-11 23:24:56 +0800

    >  compile kernel 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:24:56 up  3:45,  1 user,  load average: 0.31, 0.42, 0.51 6fef5aff6840a90423e2880f8f78cc979629579

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,7 +36,7 @@ uint32_t loader() {
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	uint8_t segbuf[4096];
+	uint8_t segbuf[9999];
 
 	int nr_ph = elf->e_phnum;
 	ph = (Elf32_Phdr *)((void *)buf + elf->e_phoff);
