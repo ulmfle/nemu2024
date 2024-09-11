51becdf04d8bad8f090b53cd3004744572af461b
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-11 22:23:23 +0800

    >  compile kernel 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:23:23 up  2:43,  1 user,  load average: 0.41, 0.29, 0.31 df4034e019c7efb79069c4b87cf8295061d943da

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,19 +36,10 @@ uint32_t loader() {
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
-	volatile uint32_t entry = elf->e_entry;
-
 	/* Load each program segment */
 	int nr_ph = elf->e_phnum;
-	int phe_size = elf->e_phentsize;
+	ph = (void *)buf + elf->e_phoff;
 
-#ifdef HAS_DEVICE
-	ide_read(buf, ELF_OFFSET_IN_DISK + elf->e_phoff, nr_ph * phe_size);
-#else
-	ramdisk_read(buf, ELF_OFFSET_IN_DISK + elf->e_phoff, nr_ph * phe_size);
-#endif
-
-	ph = (void *)buf;
 	int ph_idx;
 	for (ph_idx = 0; ph_idx < nr_ph; ++ph_idx) {
 		/* Scan the program header table, load each segment into memory */
@@ -56,15 +47,11 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-#ifdef HAS_DEVICE
-			ide_write((uint8_t*)buf + ph[ph_idx].p_offset, ph[ph_idx].p_vaddr, ph[ph_idx].p_filesz);
-#else
-			ramdisk_write((uint8_t*)buf + ph[ph_idx].p_offset, ph[ph_idx].p_vaddr, ph[ph_idx].p_filesz);
-#endif
+			memcpy((void *)ph[ph_idx].p_vaddr, (void *)(buf + ph[ph_idx].p_offset), ph[ph_idx].p_filesz);
 			/* TODO: zero the memory region
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void *)ph[ph_idx].p_vaddr + ph[ph_idx].p_filesz, 0, ph[ph_idx].p_memsz - ph[ph_idx].p_filesz);
+			memset((void *)(ph[ph_idx].p_vaddr + ph[ph_idx].p_filesz), 0, ph[ph_idx].p_memsz - ph[ph_idx].p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
@@ -75,7 +62,7 @@ uint32_t loader() {
 		}
 	}
 
-	// volatile uint32_t entry = elf->e_entry;
+	volatile uint32_t entry = elf->e_entry;
 
 #ifdef IA32_PAGE
 	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
