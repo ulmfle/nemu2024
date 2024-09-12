9f7f7acbfb52f6a329b23d24e008c0792b0c7f71
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-12 12:21:54 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:21:54 up 16:42,  1 user,  load average: 0.12, 0.33, 0.25 1dbee03bda028db66252c24525eac96b211476

--- testcase/src/extra/longlong-small.c
+++ testcase/src/extra/longlong-small.c
@@ -6,6 +6,6 @@ long long add(long long a, long long b) {
 }
 
 int main() {
-    nemu_assert(add(0x8000000000000000LL,0x7fffffffffffffffLL) == 0x7fffffffffffffffLL);
+    nemu_assert(add(0x8000000000000000LL,0x7fffffffffffffffLL) == -1);
     return 0;
 }
