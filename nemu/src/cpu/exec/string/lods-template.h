66d0f11baa98d094fbadcd9b4af112485b1931f3
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-12 15:20:31 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:20:31 up 19:41,  1 user,  load average: 0.52, 0.17, 0.19 6377a2b47fcc0cab9e6e967e502b6ee989dff443

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -4,7 +4,7 @@
 
 make_helper(concat(lods_, SUFFIX)) {
     REG(R_EAX) = REG(R_ESI);
-    REG(R_ESI) += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+    cpu.esi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
 
     print_asm("lods" str(SUFFIX) " %%ds:(%%esi),%%eax");
     return 1;
