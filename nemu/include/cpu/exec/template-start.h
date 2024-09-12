54e3ad3e96c4396438f328ca4e6aecc33acbd0cd
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-12 14:29:44 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-119-generic #129~20.04.1-Ubuntu SMP Wed Aug 7 13:07:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:29:44 up 18:50,  1 user,  load average: 0.52, 0.64, 0.41 7023a9d8647fdeb66cb83295b31ab52f5ab50674

--- nemu/include/cpu/exec/template-start.h
+++ nemu/include/cpu/exec/template-start.h
@@ -35,6 +35,6 @@
 #define MSB(n) ((DATA_TYPE)(n) >> ((DATA_BYTE << 3) - 1))
 
 // add for convenience
-#define PUSH(data) do { cpu.esp -= 4; swaddr_write(cpu.esp, 4, data); } while (0)
+#define PUSH(data) do { REG(R_ESP) -= DATA_BYTE; MEM_W(REG(R_ESP), data); } while (0)
 
-#define POP(dest) do { dest = swaddr_read(cpu.esp, 4) & (DATA_BYTE==4? ~0u : ((1 << 8*DATA_BYTE)-1)); cpu.esp += 4; } while (0)
+#define POP(dest) do { dest = MEM_R(REG(R_ESP)); REG(R_ESP) -= DATA_BYTE; } while (0)
--- nemu/src/cpu/exec/data-mov/push.c
+++ nemu/src/cpu/exec/data-mov/push.c
@@ -1,8 +1,11 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "push-template.h"
-#undef DATA_BYTE
+make_helper(push_i_b) {
+    int len = decode_i_b(eip + 1);
+    cpu.esp -= 2;
+    swaddr_write(cpu.esp, 2, (int16_t)(op_src->val));
+    return len + 1;
+}
 
 #define DATA_BYTE 2
 #include "push-template.h"
