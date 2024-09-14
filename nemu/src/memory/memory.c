585b64ec37670544b2b24326ffda72ff1e52053c
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 02:46:23 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:46:23 up  7:51,  1 user,  load average: 0.50, 0.39, 0.39 16c3b73d2d5778ea4d947016540e93ba9545dd34

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,7 +12,9 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	bool hit_l = false, hit_r = true;
 
 	val = cache_l1.read(&cache_l1, addr, len - of, &hit_l) << of;
+	Log("0x%08x len:%d ofed:%d val:0x%08x", addr, (unsigned)len, of, val);
 	if (of) val += cache_l1.read(&cache_l1, addr + len - of + 1, of, &hit_r) & ((1 << of) - 1);
+	Log("0x%08x len:%d ofed:%d val:0x%08x", addr, (unsigned)len, of, val);
 
 	if (hit_l == false || hit_r == false) {
 		val = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
@@ -20,7 +22,6 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 		// (cache_l1.replace(&cache_l1, addr, (uint8_t *)hwa_to_va(addr));
 		if (of) cache_l1.replace(&cache_l1, addr + len);
 	}
-	Log("0x%08x len:%d ofed:%d", addr, (unsigned)len, of);
 	return val;
 }
 
