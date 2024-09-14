dfd32a4fb817743c8dd2f07387d4f8b9fcedce50
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 01:41:12 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:41:12 up  6:48,  1 user,  load average: 0.50, 0.30, 0.29 38efe0695c0bc1069b25f80e22a99097d2b33df

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -31,6 +31,7 @@ static void *check_l1_hit(void *this, hwaddr_t addr) {
 }
 
 static void *l1_replace(void *this, hwaddr_t addr, uint8_t *chunk) {
+    Log("IDX: %d TAG: %x", GET_CI_L1(addr), GET_CT_L1(addr));
     int dst = random_rep(((Cache_L1 *)this)->cb_pool);
     CB_L1 *dst_cb = &(((Cache_L1 *)this)->assoc[GET_CI_L1(addr)][dst]);
     dst_cb->tag = GET_CT_L1(addr);
