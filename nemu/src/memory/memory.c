1accf7db69a7cb4f5532cbb8790bd6a1f97de1e1
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 02:56:30 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:56:30 up  8:01,  1 user,  load average: 0.00, 0.14, 0.27 ca06ca9011f08bc98f601d2a5af56113d8b3f173

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -27,7 +27,7 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 
 	bool hit_l, hit_r;
 	cache_l1.write(&cache_l1, addr, data, len - of, &hit_l);
-	if (of) cache_l1.write(&cache_l1, addr + len - of + 1, *((uint8_t *)&data + len - of), of, &hit_r);
+	if (of) cache_l1.write(&cache_l1, addr + len - of + 1, *(uint32_t *)((uint8_t *)&data + len - of), of, &hit_r);
 
 	dram_write(addr, len, data);	//write through and not write allocate
 }
