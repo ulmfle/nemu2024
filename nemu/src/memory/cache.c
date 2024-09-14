c725f9f9dc25558cd0ccdf7686246dad8a71a91f
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 00:29:27 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:29:27 up  5:37,  1 user,  load average: 3.14, 2.89, 1.63 510d247b73ce37621b6b626ee2886f0239d4e1ab

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -15,7 +15,6 @@ static uint32_t cb_l1_read(void *this, uint8_t off, size_t len) {
 }
 
 static void cb_l1_write(void *this, uint8_t off, uint8_t *data, size_t len) {
-    Log("Cache: %p write\n", this);
     uint8_t *dst = (uint8_t *)(((CB_L1 *)this)->buf);
     int idx;
     for (idx = off; idx < off + len; ++idx) {
