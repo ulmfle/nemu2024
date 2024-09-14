e97b5a8a461a01cb33802b77d513dbef4f76aa82
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 00:26:48 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:26:48 up  5:35,  1 user,  load average: 2.60, 2.23, 1.20 f7c6d9baec128ecfe6b72aaa1ee2df36c13bfe32

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -10,10 +10,12 @@ static int random_rep(void *_cb_pool) {
 }
 
 static uint32_t cb_l1_read(void *this, uint8_t off, size_t len) {
+    Log("Cache: %p read\n", this);
     return *(uint32_t *)((((CB_L1 *)this)->buf) + off) & (~((~0u) << (8*len)));
 }
 
 static void cb_l1_write(void *this, uint8_t off, uint8_t *data, size_t len) {
+    Log("Cache: %p write\n", this);
     uint8_t *dst = (uint8_t *)(((CB_L1 *)this)->buf);
     int idx;
     for (idx = off; idx < off + len; ++idx) {
