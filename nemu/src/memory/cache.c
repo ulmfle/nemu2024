d4bf72e0cfcd43bb4145f3848e866c724b68cf30
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 22:09:00 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:09:00 up 18:55,  1 user,  load average: 0.88, 1.12, 1.05 ba8513e9ec3618257cb8435f92534e8599f70b7e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -92,7 +92,7 @@ static uint32_t l1_read(Cache *this, hwaddr_t addr, size_t len, bool *hit) {
 }
 
 static void l1_write(Cache *this, hwaddr_t addr, uint32_t data, size_t len, bool *hit) {
-    l1_of = GET_CO_L1(addr) + len - CB_SIZE + 1;
+    l1_of = GET_CO_L1(addr) + len - CB_SIZE;
     l1_of = l1_of > 0 ? l1_of : 0;
     CB *cb,*cb_of = NULL;
     cb = this->check_write_hit(this, addr);
