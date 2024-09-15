a43594f98432149d94123428d8b8518004ffd62f
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 22:11:02 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:11:02 up 18:57,  1 user,  load average: 3.13, 2.08, 1.42 238dbce8be069f257adf169ba7a662ab715d484d

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -73,7 +73,7 @@ static void l1_write_replace(Cache *this, hwaddr_t addr) {
 
 static uint32_t l1_read(Cache *this, hwaddr_t addr, size_t len, bool *hit) {
     uint32_t val;
-    l1_of = GET_CO_L1(addr) + len - CB_SIZE;
+    l1_of = GET_CO_L1(addr) + len - CB_SIZE;        //attention
     l1_of = l1_of > 0 ? l1_of : 0;
 
     CB *cb,*cb_of = NULL;
@@ -92,7 +92,7 @@ static uint32_t l1_read(Cache *this, hwaddr_t addr, size_t len, bool *hit) {
 }
 
 static void l1_write(Cache *this, hwaddr_t addr, uint32_t data, size_t len, bool *hit) {
-    l1_of = GET_CO_L1(addr) + len - CB_SIZE;
+    l1_of = GET_CO_L1(addr) + len - CB_SIZE;    //attention
     l1_of = l1_of > 0 ? l1_of : 0;
     CB *cb,*cb_of = NULL;
     cb = this->check_write_hit(this, addr);
