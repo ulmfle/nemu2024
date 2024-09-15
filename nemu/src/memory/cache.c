665cbc832b6f24e17c86c13d45175cb45c542787
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-16 01:38:13 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:38:13 up 22:19,  1 user,  load average: 1.56, 1.56, 1.34 fd0000c378e35ce750a0fe2a14a435ec5041df6

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,135 +1,281 @@
-#include "cache.h"
+#include "common.h"
+#include "memory/memory.h"
+#include <stdlib.h>
+#include <time.h>       //for random
 
-int l1_of;
-int l2_of;
+#define level 2
+
+#define CB_SIZE_WIDTH 6
+
+#define NR_CL1_BLOCK_WIDTH 10
+#define ASSOC_CL1_WIDTH 3
+#define TAG_CL1_WIDTH (32 - CB_SIZE_WIDTH - NR_CL1_BLOCK_WIDTH + ASSOC_CL1_WIDTH)
+
+#define NR_CL2_BLOCK_WIDTH 12
+#define ASSOC_CL2_WIDTH 4
+#define TAG_CL2_WIDTH (32 - CB_SIZE_WIDTH - NR_CL2_BLOCK_WIDTH + ASSOC_CL2_WIDTH)
+
+#define CB_SIZE (1 << CB_SIZE_WIDTH)
+#define NR_CL1_BLOCK (1 << NR_CL1_BLOCK_WIDTH)
+#define ASSOC_CL1 (1 << ASSOC_CL1_WIDTH)
+
+#define NR_CL2_BLOCK (1 << NR_CL2_BLOCK_WIDTH)
+#define ASSOC_CL2 (1 << ASSOC_CL2_WIDTH)
+
+#define CT_L1_MASK (~0u << (32 - TAG_CL1_WIDTH))
+#define CO_L1_MASK (CB_SIZE - 1)
+#define CI_L1_MASK (((~0u) ^ (CT_L1_MASK)) ^ (concat3(CO_L, level, _MASK)))
+
+#define CT_L2_MASK (~0u << (32 - TAG_CL2_WIDTH))
+#define CO_L2_MASK (CB_SIZE - 1)
+#define CI_L2_MASK (((~0u) ^ (CT_L2_MASK)) ^ (CO_L2_MASK))
+
+#define GET_CT_L1(addr) (((addr) & CT_L1_MASK) >> (32 - TAG_CL1_WIDTH))
+#define GET_CI_L1(addr) (((addr) & CI_L1_MASK) >> CB_SIZE_WIDTH)
+#define GET_CO_L1(addr) ((addr) & concat3(CO_L, level, _MASK))
+
+#define GET_CT_L2(addr) (((addr) & CT_L2_MASK) >> (32 - TAG_CL2_WIDTH))
+#define GET_CI_L2(addr) (((addr) & CI_L2_MASK) >> CB_SIZE_WIDTH)
+#define GET_CO_L2(addr) ((addr) & CO_L2_MASK)
+
+#define GET_CT(addr, level) concat(GET_CT_L, level)(addr)
+#define GET_CI(addr, level) concat(GET_CI_L, level)(addr)
+#define GET_CO(addr, level) concat(GET_CO_L, level)(addr)
+
+#define CB_BASE \
+struct {\
+    uint8_t buf[CB_SIZE];\
+    uint32_t tag;\
+    bool valid;\
+    /*virtual*/ uint32_t (*read)(struct CB *, uint8_t, size_t);\
+    /*virtual*/ void (*write)(struct CB *, uint8_t, uint8_t *, size_t);\
+}
+
+#define CACHE_BASE \
+struct {\
+    void *cb_pool;\
+    /*virtual...=0*/ CB *(*check_read_hit)(struct Cache *, hwaddr_t);\
+    /*virtual...=0*/ CB *(*check_write_hit)(struct Cache *, hwaddr_t);\
+    /*virtual...=0*/ void (*read_replace)(struct Cache *, hwaddr_t);\
+    /*virtual...=0*/ void (*write_replace)(struct Cache *, hwaddr_t);\
+    /*virtual*/ uint32_t (*read)(struct Cache *, hwaddr_t, size_t, bool *);\
+    /*virtual*/ void (*write)(struct Cache *, hwaddr_t, uint32_t, size_t, bool *);\
+}
+
+#define NORMAL_CHECK_HIT(level) \
+    do {\
+        int idx;\
+        concat(CB_L, level) *p_cb = (concat(CB_L, level) *)(((concat(Cache_L, level) *)this)->assoc[concat(GET_CI_L, level)(addr)]);\
+        for (idx = 0; idx < concat(ASSOC_CL, level); ++idx) {\
+            if (p_cb[idx].valid && p_cb[idx].tag == concat(GET_CT_L, level)(addr)) return (CB *)(p_cb + idx);\
+        }\
+        return NULL;\
+    } while (0)
+
+#define FIND_REPLACE(level) \
+    concat(CB_L, level) *p_cb = ((concat(Cache_L, level) *)this)->assoc[GET_CI(addr, level)];\
+    int idx;\
+    for (idx = 0; idx < concat(ASSOC_CL, level); ++idx) {\
+        if (p_cb[idx].valid) continue;\
+        dst_cb = p_cb + idx;\
+        break;\
+    }\
+    if (dst_cb == NULL) {\
+        srand((unsigned)time(NULL));\
+        dst_cb = p_cb + (rand() % concat(ASSOC_CL, level));\
+    }
+
+
+typedef struct CB {
+    CB_BASE;
+} CB;
+
+typedef struct {
+    CB_BASE;
+} CB_L1;
+
+typedef struct {
+    CB_BASE;
+    bool dirty;
+} CB_L2;
+
+typedef struct Cache {
+    CACHE_BASE;
+} Cache;
+
+typedef struct {
+    CACHE_BASE;
+    CB_L1 (*assoc)[ASSOC_CL1];
+} Cache_L1;
+
+typedef struct {
+    CACHE_BASE;
+    CB_L2 (*assoc)[ASSOC_CL2];
+} Cache_L2;
+
+int of;
 
 static CB_L1 l1_block[NR_CL1_BLOCK];
-//static CB_L2 l2_block[NR_CL2_BLOCK];
+static CB_L2 l2_block[NR_CL2_BLOCK];
 
 Cache_L1 cache_l1;
 Cache_L2 cache_l2;
 
-//stand-alone
-static int random_rep(void *cb_pool) {
-    srand((unsigned)time(0));
-    return rand() % ASSOC_CL1;
-}
-
-//stand-alone
-static CB *find_replace(hwaddr_t addr, int (*method)(void *)) {
-    CB *p_cb,*dst_cb;
-    p_cb = (CB *)cache_l1.assoc[GET_CI_L1(addr)];
-
-    int dst,idx;
-    for (idx = 0; idx < ASSOC_CL1; ++idx) {
-        if (p_cb[idx].valid) continue;
-        return &p_cb[idx];
-    }
-
-    dst = method(cache_l1.cb_pool);
-    dst_cb = &p_cb[dst];
-    return dst_cb;
-}
-
-//stand-alone
-static CB *check_hit(Cache *this, hwaddr_t addr) {
-    int idx;
-    CB *p_cb = (CB *)(((Cache_L1 *)this)->assoc[GET_CI_L1(addr)]);
-    for (idx = 0; idx < ASSOC_CL1; ++idx) {
-        if (p_cb[idx].valid && p_cb[idx].tag == GET_CT_L1(addr)) return (p_cb + idx);
-    }
-    return NULL;
-}
-
-
-static uint32_t cb_read(CB *this, uint8_t off, size_t len) {
+//base
+static uint32_t cbread(CB *this, uint8_t off, size_t len) {
     return (*(uint32_t *)(this->buf + off)) & (~0u >> ((4 - len) << 3));
 }
 
-static void cb_write(CB *this, uint8_t off, uint8_t *data, size_t len) {
+//base
+static void cbwrite(CB *this, uint8_t off, uint8_t *data, size_t len) {
     memcpy(this->buf + off, data, len);
 }
 
-static CB *l1_check_read_hit(Cache *this, hwaddr_t addr) {
-    return check_hit(this, addr);
-}
-
-static CB *l1_check_write_hit(Cache *this, hwaddr_t addr) {
-    return check_hit(this, addr);
-}
-
-static void l1_replace(Cache *this, hwaddr_t addr) {
-    CB *dst_cb;
-    dst_cb = find_replace(addr, random_rep);
-    dst_cb->tag = GET_CT_L1(addr);
-    dst_cb->valid = 1;
-    dst_cb->write(dst_cb, 0, hwa_to_va((addr & (~CO_L1_MASK))), CB_SIZE);
-    if (l1_of != 0) {
-        l1_of = 0;
-        l1_replace(this, addr + CO_L1_MASK + 1);
-    }
-}
-
-static void l1_read_replace(Cache *this, hwaddr_t addr) {
-    l1_replace(this, addr);
-}
-
-static void l1_write_replace(Cache *this, hwaddr_t addr) {
-    l1_replace(this, addr);
-}
-
-static uint32_t l1_read(Cache *this, hwaddr_t addr, size_t len, bool *hit) {
+//base
+static uint32_t cread(Cache *this, hwaddr_t addr, size_t len, bool *hit) {
     uint32_t val;
-    l1_of = GET_CO_L1(addr) + len - CB_SIZE;        //attention
-    l1_of = l1_of > 0 ? l1_of : 0;
-
     CB *cb,*cb_of = NULL;
-    cb = this->check_read_hit((Cache *)&cache_l1, addr);
-    if (l1_of) cb_of = this->check_read_hit((Cache *)&cache_l1, addr + len);
-    if (((l1_of == 0) && (cb == NULL)) || ((l1_of > 0) && (cb == NULL || cb_of == NULL))) {
+    of = GET_CO_L1(addr) + len - CB_SIZE;
+    of = of > 0 ? of : 0;
+    cb = this->check_read_hit(this, addr);
+    if (of) cb_of = this->check_read_hit(this, addr + len);
+    if (((of == 0) && (cb == NULL)) || ((of > 0) && (cb == NULL || cb_of == NULL))) {
         *hit = false;
         return 0;
     }
     *hit = true;
-    val = cb->read(cb, GET_CO_L1(addr), len - l1_of);
-    if (l1_of) val += (cb_of->read(cb_of, 0, l1_of) << ((len - l1_of) << 3));
-
-    l1_of = 0;
+    val = cb->read(cb, GET_CO_L1(addr), len - of);
+    if (of) val += (cb_of->read(cb_of, 0, of) << ((len - of) << 3));
+    of = 0;
     return val;
 }
 
-static void l1_write(Cache *this, hwaddr_t addr, uint32_t data, size_t len, bool *hit) {
-    l1_of = GET_CO_L1(addr) + len - CB_SIZE;    //attention
-    l1_of = l1_of > 0 ? l1_of : 0;
+//base
+static void cwrite(Cache *this, hwaddr_t addr, uint32_t data, size_t len, bool *hit) {
     CB *cb,*cb_of = NULL;
-    cb = this->check_write_hit(this, addr);
-    if (l1_of) cb_of = this->check_write_hit((Cache *)&cache_l1, addr + len);
-    if (((l1_of == 0) && (cb == NULL)) || ((l1_of > 0) && (cb == NULL || cb_of == NULL))) {
+    of = GET_CO_L1(addr) + len - CB_SIZE;
+    of = of > 0 ? of : 0;
+    cb = this->check_read_hit(this, addr);
+    if (of) cb_of = this->check_read_hit(this, addr + len);
+    if (((of == 0) && (cb == NULL)) || ((of > 0) && (cb == NULL || cb_of == NULL))) {
         *hit = false;
         return;
     }
     *hit = true;
-
-    uint8_t *of_data = ((uint8_t *)&data) + len - l1_of;
-    cb->write(cb, GET_CO_L1(addr), (uint8_t *)&data, len - l1_of);
-    if (l1_of) cb_of->write(cb_of, 0, of_data, l1_of);
-
-    l1_of = 0;
+    uint8_t *of_data = ((uint8_t *)&data) + len - of;
+    cb->write(cb, GET_CO_L1(addr), (uint8_t *)&data, len - of);
+    if (of) cb_of->write(cb_of, 0, of_data, of);
+    of = 0;
 }
 
-static void cache_l1_init(Cache *this) {
-    CACHE_INIT(1);
-    CB_INIT(1);
-    ((Cache_L1 *)this)->assoc = (CB_L1 (*)[8])l1_block;
+static CB *l1_check_read_hit(Cache *this, hwaddr_t addr) {
+    NORMAL_CHECK_HIT(1);
 }
 
-// static void cache_l2_init(Cache *this) {
-//     CACHE_INIT(2);
-//     CB_INIT(2);
+static CB *l1_check_write_hit(Cache *this, hwaddr_t addr) {
+    NORMAL_CHECK_HIT(1);
+}
 
-// }
+static void l1_read_replace(Cache *this, hwaddr_t addr) {
+
+}
+
+static void l1_write_replace(Cache *this, hwaddr_t addr) {
+    return;                                                     //write through and not write allocate
+}
+
+static CB *l2_check_read_hit(Cache *this, hwaddr_t addr) {
+    NORMAL_CHECK_HIT(2);
+}
+
+static CB *l2_check_write_hit(Cache *this, hwaddr_t addr) {
+    int idx;
+    CB_L2 *p_cb = (CB_L2 *)(((Cache_L2 *)this)->assoc[GET_CI(addr, 2)]);
+    for (idx = 0; idx < ASSOC_CL2; ++idx) {
+        if (p_cb[idx].valid && p_cb[idx].tag == GET_CT(addr, 2)) {
+            p_cb[idx].dirty = 1;
+            return (CB *)(p_cb + idx);
+        }
+    }
+    return NULL;
+}
+
+static void l2_read_replace(Cache *this, hwaddr_t addr) {
+    CB_L2 *dst_cb = NULL;
+    FIND_REPLACE(2);
+    dst_cb->tag = GET_CT(addr, level);
+    dst_cb->valid = 1;
+    dst_cb->write((CB *)dst_cb, 0, hwa_to_va((addr & (~CB_SIZE))), CB_SIZE);
+    if (of != 0) {
+        of = 0;
+        l2_read_replace(this, addr + CB_SIZE + 1);
+    }
+}
+
+static void l2_write_replace(Cache *this, hwaddr_t addr) {
+    
+}
+
+static void init_cache_internal() {
+    cache_l1.cb_pool = (void *)l1_block;
+    cache_l1.check_read_hit = l1_check_read_hit;
+    cache_l1.check_write_hit = l1_check_write_hit;
+    cache_l1.read_replace = l1_read_replace;
+    cache_l1.write_replace = l1_write_replace;
+    cache_l1.assoc = (CB_L1 (*)[8])cache_l1.cb_pool;
+
+    cache_l2.cb_pool = (void *)l2_block;
+    cache_l2.check_read_hit = l2_check_read_hit;
+    cache_l2.check_write_hit = l2_check_write_hit;
+    cache_l2.read_replace = l2_read_replace;
+    cache_l2.write_replace = l2_write_replace;
+    cache_l2.assoc = (CB_L2 (*)[16])cache_l2.cb_pool;
+
+    cache_l1.read = cache_l2.read = cread;
+    cache_l1.write = cache_l2.write = cwrite;
+
+    int l1_idx;
+    for (l1_idx = 0; l1_idx < NR_CL1_BLOCK; ++l1_idx) {
+        l1_block[l1_idx].read = cbread;
+        l1_block[l1_idx].write = cbwrite;
+        l1_block[l1_idx].valid = 0;
+    }
+    int l2_idx;
+    for (l2_idx = 0; l2_idx < NR_CL2_BLOCK; ++l2_idx) {
+        l2_block[l2_idx].read = cbread;
+        l2_block[l2_idx].write = cbwrite;
+        l2_block[l2_idx].valid = 0;
+    }
+}
 
 void init_cache() {
-    cache_l1.init = cache_l1_init;
-    cache_l1.init((Cache *)&cache_l1);
+    init_cache_internal();
+}
+
+//main
+uint32_t cache_read(hwaddr_t addr, size_t len, bool *hit) {
+    uint32_t val;
+    bool hit_l1, hit_l2;
+    val = cache_l1.read((Cache *)&cache_l1, addr, len, &hit_l1);
+    if (hit_l1 == 0) {
+        val = cache_l2.read((Cache *)&cache_l2, addr, len, &hit_l2);
+        cache_l1.read_replace((Cache *)&cache_l1, addr);
+    } else {
+        *hit = true;
+        return val;
+    }
+
+    if (hit_l2 == 0) {
+        *hit = false;
+        return 0;
+    } else {
+        *hit = true;
+        return val;
+    }
+}
+
+//main
+void cache_replace(hwaddr_t addr) {
+    cache_l2.read_replace((Cache *)&cache_l2, addr);
+    cache_l1.read_replace((Cache *)&cache_l1, addr);
 }
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,26 +1,24 @@
 #include "common.h"
-#include "cache.h"
 
+uint32_t cache_read(hwaddr_t, size_t, bool *);
+void cache_replace(hwaddr_t);
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	uint32_t val;
-	bool hit_l1=0;
-	val = cache_l1.read((Cache *)&cache_l1, addr, len, &hit_l1);
-
-	if (hit_l1 == 0) {
+	bool hit;
+	val = cache_read(addr, len, &hit);
+	if (hit == false) {
 		val = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-		cache_l1.read_replace((Cache *)&cache_l1, addr);
+		cache_replace(addr);
 	}
 	return val;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	bool hit_l1;
-	cache_l1.write((Cache *)&cache_l1, addr, data, len, &hit_l1);
-	dram_write(addr, len, data);	//write through and not write allocate
+	dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
