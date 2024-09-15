64c383cd3356ad05a49bb3d1c2a560252f808707
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 21:12:00 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:12:00 up 18:00,  1 user,  load average: 1.28, 0.71, 0.43 c57be2f98701a5b398b8fb941896df73459d057

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -90,7 +90,7 @@ static uint32_t l1_read(Cache *this, hwaddr_t addr, size_t len, bool *hit) {
     Log("");
     val = cb->read(cb, GET_CO_L1(addr), len - l1_of);
     Log("val:%08x",val);
-    if (l1_of) val ^= (cb_of->read(cb_of, 0, l1_of) << ((len - l1_of) << 3));
+    if (l1_of) val += (cb_of->read(cb_of, 0, l1_of) << ((len - l1_of) << 3));
 
     l1_of = 0;
     return val;
