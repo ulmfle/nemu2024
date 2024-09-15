5b248bc2bd386069c25a48591ad335bd0e5683a1
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 18:58:28 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:58:28 up 15:50,  1 user,  load average: 1.63, 1.11, 0.95 abf67d9c3aa9e74a8264ad72d5af8b4827cc4e8

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -28,7 +28,7 @@ static CB *find_replace(hwaddr_t addr, int (*method)(void *)) {
 static uint32_t cb_read(CB *this, uint8_t off, size_t len) {
     int idx;
     for (idx = 0; idx < CB_SIZE; ++idx) printf("%02x ",this->buf[idx]);
-    Log("off:%d val: 0x%08x len:%u",off,(*(uint32_t *)(this->buf + off)) & ((1 << (len << 3)) - 1), (unsigned)len);
+    Log("off:%d val: 0x%08x len:%u",off,(*(uint32_t *)(this->buf + off)), (unsigned)len);
     return (*(uint32_t *)(this->buf + off)) & ((1 << (len << 3)) - 1);
 }
 
