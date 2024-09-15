d518187c92480018502ed5692c94f0ea96c9a41a
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-15 14:58:42 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:58:42 up 11:57,  1 user,  load average: 1.01, 1.66, 2.25 32757aa32ff0e1fcbf2629e28f5f478f4111300

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -73,6 +73,8 @@ static uint32_t l1_read(void *this, hwaddr_t addr, size_t len, bool *hit) {
     cb = (CB_L1 *)(((Cache_L1 *)this)->check_hit(&cache_l1, addr));
     if (l1_of) cb_of = (CB_L1 *)(((Cache_L1 *)this)->check_hit(&cache_l1, addr + len));
 
+    Log("of %d", l1_of);
+
     if (((l1_of == 0) && (cb == NULL)) || ((l1_of > 0) && (cb == NULL || cb_of == NULL))) {
         *hit = false;
         return 0;
