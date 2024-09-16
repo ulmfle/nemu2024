1a1a61d525587f24f353fd7568f6373ed9b38c6f
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-16 11:17:58 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:17:58 up 1 day,  7:54,  1 user,  load average: 2.06, 1.16, 0.77 cfa039601f09aabdb2cc3a136415e3c5d68df85

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -64,10 +64,10 @@ struct {\
 
 #define NORMAL_CHECK_HIT(level) \
     do {\
+        printf("\n");\
         int idx;\
         concat(CB_L, level) *p_cb = (concat(CB_L, level) *)(((concat(Cache_L, level) *)this)->assoc[concat(GET_CI_L, level)(addr)]);\
         for (idx = 0; idx < concat(ASSOC_CL, level); ++idx) {\
-            printf("\n");\
             Log("valid: %d tag: 0x%08x hit_tag: 0x%08x", p_cb[idx].valid, p_cb[idx].tag, concat(GET_CT_L, level)(addr));\
             if (p_cb[idx].valid && p_cb[idx].tag == concat(GET_CT_L, level)(addr)) return (CB *)(p_cb + idx);\
         }\
