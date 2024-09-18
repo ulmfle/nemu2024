9ab286e298247f9000f983c69f65da5026b71c76
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-18 12:07:02 +0800

    >  compile kernel 3023001461 uux Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:07:02 up  1:33,  1 user,  load average: 0.20, 0.27, 0.27 2cd4dd9a9018670a4ad5242a8e8cf0b338e55f42

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -65,8 +65,7 @@ FLOAT f2F(float a) {
 }
 
 FLOAT Fabs(FLOAT a) {
-	//return a ^ (1 << 31) ? a : (~a+1) & (~0u >> 1);
-	return a > 0 ? a : -a;
+	return a >= 0 ? a : (~a+1) & (~0u >> 1);
 }
 
 /* Functions below are already implemented */
