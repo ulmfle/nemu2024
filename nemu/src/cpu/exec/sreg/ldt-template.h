119cf4cf260fe3403ab21132b89b89297549e2a9
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-20 22:23:13 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:23:13 up 13:51,  1 user,  load average: 0.07, 0.08, 0.02 ae5ea8e7dbdaa2e365df2c4ddc771c9780befda

--- nemu/src/cpu/exec/sreg/ldt-template.h
+++ nemu/src/cpu/exec/sreg/ldt-template.h
@@ -6,7 +6,7 @@ make_helper(concat(lgdt_, SUFFIX)) {
     int len = concat(decode_rm_, SUFFIX)(eip + 1);
 
     cpu.gdtr.limit = swaddr_read(op_src->addr, 2);
-    cpu.gdtr.LBA = swaddr_read(op_src->addr, 4) & (~0u >> (DATA_BYTE == 2 ? 8 : 0));
+    cpu.gdtr.LBA = swaddr_read(op_src->addr + 2, 4) & (~0u >> (DATA_BYTE == 2 ? 8 : 0));
 
     print_asm_template1();
     return len + 1;
