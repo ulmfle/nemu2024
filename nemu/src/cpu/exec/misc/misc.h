745cb6adddaeca5c32b31c867d3bc44001497708
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-24 15:26:55 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:26:55 up 17:15,  1 user,  load average: 0.01, 0.02, 0.00 effed215813f75e9dae9b595e7ede2b98a711f8e

--- nemu/src/cpu/exec/misc/misc.h
+++ nemu/src/cpu/exec/misc/misc.h
@@ -6,6 +6,13 @@ make_helper(int3);
 make_helper(lea);
 
 make_helper(int_b);
+make_helper(stc);
+make_helper(sti);
 make_helper(std);
+make_helper(cld);
+make_helper(clc);
+make_helper(cli);
+make_helper(clts);
+make_helper(cmc);
 
 #endif
