7d2c5ba90636b85a8ba05b124a78f91ac7e0cd8f
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-25 19:15:25 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:15:25 up  5:33,  0 users,  load average: 0.22, 0.05, 0.01 90e00e1041790201f77d5166fafb15e5e00d313

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -34,17 +34,18 @@ void init_page(void) {
 	 * If you do not understand it, refer to the C code below.
 	 */
 
-	asm volatile ("std;\
-	 1: stosl;\
-		subl %0, %%eax;\
-		jge 1b;\
-		cld" : :
-		"i"(PAGE_SIZE), "a"((PHY_MEM - PAGE_SIZE) | 0x7), "D"(ptable - 1));
+		uint32_t pframe_addr = PHY_MEM - PAGE_SIZE;
+		ptable = (PTE *)va_to_pa(kptable) + PHY_MEM / PAGE_SIZE - 1;
 
+		// fill PTEs reversely
+		for (; pframe_addr >= 0; pframe_addr -= PAGE_SIZE) {
+			ptable->val = make_pte(pframe_addr);
+			ptable --;
+		}
 	/*
 		===== referenced code for the inline assembly above =====
 
-		int32_t pframe_addr = PHY_MEM - PAGE_SIZE;
+		uint32_t pframe_addr = PHY_MEM - PAGE_SIZE;
 		ptable --;
 
 		// fill PTEs reversely
