bf56674dd1cfd986a5560403e747672d9de74aaa
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-25 18:47:06 +0800

    >  compile kernel 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:47:06 up  5:04,  0 users,  load average: 0.05, 0.02, 0.00 c51eef59a913f4795515daa18a8322e4d8717f42

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -7,6 +7,7 @@ static PDE kpdir[NR_PDE] align_to_page;						// kernel page directory
 static PTE kptable[PHY_MEM / PAGE_SIZE] align_to_page;		// kernel page tables
 
 PDE* get_kpdir() { return kpdir; }
+PTE* get_kptable() { return kptable; }
 
 /* set up page tables for kernel */
 void init_page(void) {
@@ -32,7 +33,7 @@ void init_page(void) {
 	/* We use inline assembly here to fill PTEs for efficiency.
 	 * If you do not understand it, refer to the C code below.
 	 */
-	
+
 	asm volatile ("std;\
 	 1: stosl;\
 		subl %0, %%eax;\
