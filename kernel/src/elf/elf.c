898288af859374802da46e092aafb48de9bf7e97
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-29 04:50:32 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:50:32 up 3 days,  4:08,  1 user,  load average: 0.60, 0.86, 0.60 93251d1fe10dddf568fdd0467c6e7d57a697abc

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -20,12 +20,12 @@ uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
-	uint8_t buf[4096];
+	uint8_t buf[40960];
 
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
-	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 40960);
 #endif
 
 	elf = (void*)buf;
