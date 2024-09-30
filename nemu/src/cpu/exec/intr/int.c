1e11ad9fef661209978fb2dddc6e158cf0087104
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-30 21:41:22 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:41:22 up 4 days, 13:17,  1 user,  load average: 1.49, 1.27, 1.30 8ee8089db28943565d570ddf2933afe8e597f5b

--- nemu/src/cpu/exec/intr/int.c
+++ nemu/src/cpu/exec/intr/int.c
@@ -2,9 +2,9 @@
 #include "cpu/exec/helper.h"
 
 make_helper(int_b) {
-	int len = decode_i_b(eip + 1);
+	decode_i_b(eip + 1);
 	print_asm("int %s", op_src->str);
-	cpu.eip += len + 1;
+	cpu.eip += 2;
 	raise_intr(op_src->val);
 
 	return 0;
