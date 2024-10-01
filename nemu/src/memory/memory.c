5457c5789d63b999562960fea54cc86fbdf3a1d3
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-02 00:12:21 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:12:21 up 12:11,  1 user,  load average: 0.57, 0.35, 0.29 cd70adf7e69549db4cb4ccf15a3a41648ed77f7a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -63,7 +63,7 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	PTE pte;
 	pde.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + sizeof(uint32_t)*(addr >> 22), sizeof(uint32_t));
 	Assert(pde.present, "addr : %08x", addr);
-	pte.val = hwaddr_read((pde.page_frame << 12) + sizeof(uint32_t)*(addr >> 12), sizeof(uint32_t)); //attention
+	pte.val = hwaddr_read((pde.page_frame << 12) + sizeof(uint32_t)*((addr >> 12) & ~(~0u << 10)), sizeof(uint32_t)); //attention
 	Assert(pte.present, "addr : %08x", addr);
 	return (pte.page_frame << 12) + (addr & PAGE_MASK);
 }
