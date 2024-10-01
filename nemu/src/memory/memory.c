562d9973ee5684d3744aca9eaf53f39b5d0369af
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-01 12:51:23 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:51:23 up 54 min,  1 user,  load average: 0.25, 0.45, 0.50 4ed61d325042f240e8a3cda099baea8c9f314d9

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -99,9 +99,9 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	if (hit == true && f_ret == false) return (pte.page_frame << 12) + (addr & PAGE_MASK);
 
 	pde.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + sizeof(uint32_t)*(addr >> 22), sizeof(uint32_t)); 
-	assert(pde.present);
+	Assert(pde.present, "addr : %08x", addr);
 	pte.val = hwaddr_read((pde.page_frame << 12) + sizeof(uint32_t)*((addr >> 12) & ~(~0u << 10)), sizeof(uint32_t)); //attention
-	assert(pte.present);
+	Assert(pte.present, "addr : %08x", addr);
 	tlb_replace(addr, pte.val);
 	return (pte.page_frame << 12) + (addr & PAGE_MASK);
 }
