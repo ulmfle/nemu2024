4f5eea60af196c21858cdc9f2ef22fa8cf742373
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-01 12:46:20 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:46:20 up 49 min,  1 user,  load average: 0.38, 0.46, 0.49 1a046fe7acb955b68be409c4f06431876d32b17f

--- testcase/src/matrix-mul.c
+++ testcase/src/matrix-mul.c
@@ -316,7 +316,7 @@ int main() {
 		for(j = 0; j < N; j ++) {
 			c[i][j] = 0;
 			for(k = 0; k < N; k ++) {
-				asm volatile ("int $0x3");
+				//asm volatile ("int $0x3");
 				c[i][j] += a[i][k] * b[k][j];
 				loop ++;
 			}
