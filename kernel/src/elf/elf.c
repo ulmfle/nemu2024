26d9221a539beb0068e4159826bcd56b83728bdf
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-01 22:14:49 +0800

    >  compile kernel 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:14:49 up 10:14,  1 user,  load average: 0.16, 0.28, 0.34 51d2f7a012c955b6fe351e914dab9e1cd750e41

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -46,10 +46,11 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
+			set_bp();
 			uint8_t *pmalloc = (uint8_t *)mm_malloc(ph[ph_idx].p_vaddr, ph[ph_idx].p_memsz);
 
 #ifdef HAS_DEVICE
-			ide_read((uint8_t *)(ph[ph_idx].p_vaddr), ELF_OFFSET_IN_DISK + ph[ph_idx].p_offset, ph[ph_idx].p_filesz);
+			ide_read(pmalloc, ELF_OFFSET_IN_DISK + ph[ph_idx].p_offset, ph[ph_idx].p_filesz);
 #else
 			ramdisk_read(pmalloc, ELF_OFFSET_IN_DISK + ph[ph_idx].p_offset, ph[ph_idx].p_filesz);
 #endif
