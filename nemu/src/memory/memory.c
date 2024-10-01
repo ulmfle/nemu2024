ab9b188f6e6ffb69efbc37c8d322ac34c6b8ecc2
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-01 14:31:17 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:31:17 up  2:33,  1 user,  load average: 0.29, 0.39, 0.51 c2ec81862b63d91521a041c963cbb5742515373f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -18,6 +18,7 @@ void lnread64(lnaddr_t, void *);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
+	if (addr == 0x1010fa0) {Log("dest read");}
 #ifdef HAS_DEVICE
 	int map;
 	if ((map = is_mmio(addr)) != -1) {
@@ -37,6 +38,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
+	if (addr == 0x1010fa0) {Log("dest write, data: %08x", data);}
 #ifdef HAS_DEVICE
 	int map;
 	if ((map = is_mmio(addr)) != -1) {
