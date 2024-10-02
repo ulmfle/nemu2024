86a387ad07fa0c3bac2c066612f933075aaa8fa6
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-02 21:22:31 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:22:31 up  4:56,  1 user,  load average: 4.15, 1.76, 1.13 c493f0993c77ad0d17f8daca445972dd43959031

--- nemu/src/cpu/exec/intr/intr.c
+++ nemu/src/cpu/exec/intr/intr.c
@@ -22,7 +22,6 @@ void pop64(uint64_t *dst) {
 
 void raise_intr(uint8_t NO) {
     if (!cpu.eflags.IF) return;
-    Log("intr eip:%08x", cpu.eip);
     GateDesc gd;
 	_lptr lptr;
 	lptr.ptr = cpu.eip;
