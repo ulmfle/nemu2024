118b0671fddb1709a728773014786663a0ab7ee1
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-04 00:48:47 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:48:47 up  4:53,  1 user,  load average: 0.88, 0.93, 1.14 413e88c76e3f53d4ed4b7976ff293d5f520f3644

--- nemu/src/cpu/exec/intr/hlt.c
+++ nemu/src/cpu/exec/intr/hlt.c
@@ -3,12 +3,11 @@
 #include "cpu/exec/helper.h"
 
 make_helper(hlt) {
+    uint32_t intr_NO = 0;
     print_asm("hlt");
-    if (cpu.INTR & cpu.eflags.IF) {
-        cpu.eip += 1;
-        uint32_t intr_NO = i8259_query_intr();
-        i8259_ack_intr();
-        raise_intr(intr_NO);
-    }
-    return 0;       //loop when no == -1
+    while (!(cpu.INTR & cpu.eflags.IF)) intr_NO = i8259_query_intr();
+    cpu.eip += 1;
+    i8259_ack_intr();
+    raise_intr(intr_NO);
+    return 0;
 }
