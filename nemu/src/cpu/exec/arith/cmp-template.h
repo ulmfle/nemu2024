dd97eb7980055aec39526fb94f47973ff251261f
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-08 13:05:53 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:05:53 up  2:43,  1 user,  load average: 0.41, 0.13, 0.04 42c70c8dbe4ddfa1c91d4e4ecc3718f2f6d92b1a

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,7 +3,7 @@
 #define instr cmp
 
 static void do_execute() {
-    DATA_TYPE result = op_dest->val - op_src->val;
+    DATA_TYPE result = op_dest->val - (DATA_TYPE_S)op_src->val;
 
     update_eflags_pf_zf_sf((DATA_TYPE_S)result);
 	cpu.eflags.CF = ((op_dest->val >> (8*DATA_BYTE - 2)) & 1) ^ ((result >> (8*DATA_BYTE - 2)) & 1);
@@ -22,7 +22,7 @@ make_instr_helper(rm2r);
 make_helper(concat(cmp_i2eax_, SUFFIX)) {
     int len = concat(decode_i_, SUFFIX)(eip + 1);
 
-    DATA_TYPE result = REG(0) - op_src->val;
+    DATA_TYPE result = REG(0) - (DATA_TYPE_S)op_src->val;
     update_eflags_pf_zf_sf((DATA_TYPE_S)result);
 	cpu.eflags.CF = ((REG(0) >> (8*DATA_BYTE - 2)) & 1) ^ ((result >> (8*DATA_BYTE - 2)) & 1);
     cpu.eflags.OF = MSB((REG(0) ^ op_src->val) & (REG(0) ^ result));
