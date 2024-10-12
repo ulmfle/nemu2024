81f0d54fc726026d0469d61eee8d83e6d9de2428
tracer-NEMU2020 <tjuics2020@163.com>
2024-10-12 16:36:12 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:36:12 up 1 day,  8:26,  1 user,  load average: 0.68, 0.78, 0.64 99775468d3f1a0b2e755c1ef89de74557b1d4ac9

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -4,12 +4,12 @@
 
 #if DATA_BYTE == 1
 make_helper(push_i_b) {
-    int len = decode_i_b(eip + 1);
-    cpu.esp -= 4;if (cpu.eip == 0x806b164) Log("%08x", op_src->simm);
-    swaddr_write(cpu.esp, 4, SR_SS, (int32_t)(op_src->val));
+    int8_t val = instr_fetch(eip + 1, 1);
+    cpu.esp -= 4;if (cpu.eip == 0x806b164) Log("%08x", (int32_t)val);
+    swaddr_write(cpu.esp, 4, SR_SS, (int32_t)val);
 
-    print_asm_template1();
-    return len + 1;
+    print_asm("push" str(SUFFIX) " 0x%08x", (int32_t)val);
+    return 2;
 }
 #endif
 
