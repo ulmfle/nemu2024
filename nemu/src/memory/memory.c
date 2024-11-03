b099201c54583a1ae385fdeec48879b90f487ea9
tracer-NEMU2020 <tjuics2020@163.com>
2024-11-03 17:29:26 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-124-generic #134~20.04.1-Ubuntu SMP Tue Oct 1 15:27:33 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:29:26 up 22 min,  1 user,  load average: 0.50, 0.39, 0.47 25853e6f51a497d856425db61f586e597ece50b8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -98,9 +98,9 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	int f_ret = tlb_flush();
 	if (hit == true && f_ret == false) return (pte.page_frame << 12) + (addr & PAGE_MASK);
 
-	pde.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + sizeof(uint32_t)*(addr >> 22), sizeof(uint32_t)); 
+	pde.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + sizeof(uint32_t)*(addr >> 22), sizeof(PDE)); 
 	assert(pde.present);
-	pte.val = hwaddr_read((pde.page_frame << 12) + sizeof(uint32_t)*((addr >> 12) & ~(~0u << 10)), sizeof(uint32_t)); //attention
+	pte.val = hwaddr_read((pde.page_frame << 12) + sizeof(uint32_t)*((addr >> 12) & ~(~0u << 10)), sizeof(PTE)); //attention
 	assert(pte.present);
 	tlb_replace(addr, pte.val);
 	return (pte.page_frame << 12) + (addr & PAGE_MASK);
