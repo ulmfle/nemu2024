2b7718bd6634659b7c6411cb438e0d1e22416fba
tracer-NEMU2020 <tjuics2020@163.com>
2024-11-08 19:50:37 +0800

    >  run 3023001461 uux Linux ubuntu 5.15.0-124-generic #134~20.04.1-Ubuntu SMP Tue Oct 1 15:27:33 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:50:37 up 2 days,  2:33,  1 user,  load average: 0.30, 0.08, 0.03 fbb69b8e9711a55e9f4738ea20ba5526b75e580

--- testcase/src/extra/screenwrite.c
+++ testcase/src/extra/screenwrite.c
@@ -3,17 +3,26 @@
 #include <string.h>
 #include <stdint.h>
 
-#define vmem 0xa0000
+#define vmem_addr 0xa0000
 #define SCR_SIZE (320 * 200)
 #define CTR_COL 320
 #define CTR_ROW 200
 
+uint8_t (*vmem)[CTR_COL] = (void *)vmem_addr;
+
 inline void setpix(int x, int y, uint8_t color) {
-    memcpy((void *)vmem+CTR_ROW*y+x, &color, 1);
+    vmem[y-1][x-1] = color;
 }
 
 int main() {
     setpix(3, 3, 0xff);
+    setpix(4, 4, 0xff);
+    setpix(5, 4, 0xff);
+    setpix(5, 3, 0xff);
+    setpix(6, 3, 0xff);
+    setpix(7, 3, 0xff);
+    setpix(8, 3, 0xff);
+    setpix(9, 3, 0xff);
     HIT_GOOD_TRAP;
     return 0;
 }
