9d2e27b081cf04bba425f22410f44534d2d0fb09
tracer-NEMU2020 <tjuics2020@163.com>
2024-11-27 00:53:20 +0800

    >  compile NEMU 3023001461 uux Linux ubuntu 5.15.0-125-generic #135~20.04.1-Ubuntu SMP Mon Oct 7 13:56:22 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:53:20 up  5:50,  1 user,  load average: 0.54, 0.45, 0.45 5ebeb1b57d23e2c7aa2f26534bc9c0f8202f7a3

--- nemu/src/cpu/exec/bit/bt-template.h
+++ nemu/src/cpu/exec/bit/bt-template.h
@@ -3,7 +3,7 @@
 #define instr bt
 
 static void do_execute() {
-    cpu.eflags.CF = op_dest->val & (1 << op_src->val);
+    cpu.eflags.CF = (op_dest->val >> op_src->val) & 1;
 
     print_asm_template2();
 }
