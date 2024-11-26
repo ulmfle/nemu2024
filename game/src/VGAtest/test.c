be8750d96cde2868640e9263f3895b546b48db15
tracer-NEMU2020 <tjuics2020@163.com>
2024-11-26 23:50:20 +0800

    >  compile game 3023001461 uux Linux ubuntu 5.15.0-125-generic #135~20.04.1-Ubuntu SMP Mon Oct 7 13:56:22 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:50:20 up  4:47,  1 user,  load average: 0.60, 0.39, 0.31 5787231cf19de5b67082be50855ad0e4e19f0a98

---
+++ game/src/VGAtest/test.c
@@ -0,0 +1,18 @@
+#include "device/video.h"
+
+volatile int tick = 0;
+
+void
+timer_event(void) {
+	tick ++;
+}
+
+void
+keyboard_event(void){
+}
+
+void main_loop() {
+    while (true) {
+        draw_string("Success!", 0, 0, 0x33);
+    }
+}
