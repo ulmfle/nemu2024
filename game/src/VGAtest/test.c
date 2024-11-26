a4f726214a16364bccfc433d5de216f099b032d7
tracer-NEMU2020 <tjuics2020@163.com>
2024-11-27 00:16:22 +0800

    >  compile game 3023001461 uux Linux ubuntu 5.15.0-125-generic #135~20.04.1-Ubuntu SMP Mon Oct 7 13:56:22 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:16:22 up  5:13,  1 user,  load average: 0.57, 0.36, 0.53 12d826b5b85ae6cd8f7b4b3c3f8aba97f819988

--- game/src/VGAtest/test.c
+++ game/src/VGAtest/test.c
@@ -13,7 +13,13 @@ keyboard_event(void){
 
 void main_loop() {
     while (true) {
-        draw_string("Success!", 0, 0, 0x33);
+        int idx,jdx;
+        // draw_string("Success!", 0, 0, 0x33);
+        for (idx = 0; idx < SCR_HEIGHT/2; ++idx) {
+            for (jdx = 0; jdx < SCR_WIDTH/2; ++jdx) {
+                draw_pixel(idx, jdx, 0x33);
+            }
+        }
         display_buffer();
     }
 }
