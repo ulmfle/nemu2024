bfd88b825ac4fcd7335bce38d72923272c7a4c1a
tracer-NEMU2020 <tjuics2020@163.com>
2024-11-26 21:53:13 +0800

    >  compile game 3023001461 uux Linux ubuntu 5.15.0-125-generic #135~20.04.1-Ubuntu SMP Mon Oct 7 13:56:22 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:53:13 up  2:50,  1 user,  load average: 0.47, 0.50, 0.34 d40dc335572024f5b1565efa3e0beb55c95b76e

--- game/include/device/video.h
+++ game/include/device/video.h
@@ -13,7 +13,8 @@ extern uint8_t *vmem;
 static inline void
 draw_pixel(int x, int y, int color) {
 	assert(x >= 0 && y >= 0 && x < SCR_HEIGHT && y < SCR_WIDTH);
-	vmem[(x << 8) + (x << 6) + y] = color;
+	//vmem[(x << 8) + (x << 6) + y] = color;
+	((uint8_t (*)[SCR_WIDTH])vmem)[x][y] = color;
 }
 
 void prepare_buffer(void);
